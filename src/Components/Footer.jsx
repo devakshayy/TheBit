c33148f5efb3127aec4b9dba896505f842c691a2
devakshayy <devakshay916@gmail.com>
2024-12-17 12:34:21 +0530

    logo changed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from '../assets/TheBitLogo.png'
+import logo from '../assets/minnuLogo.png'
 
 
 const Footer = () => {
